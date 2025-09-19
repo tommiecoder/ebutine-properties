f0f1102f1609082012a1c418bd050ab8f781d185
Tomiwa Adeoye <101461744+tommiecoder@users.noreply.github.com>
2025-09-20 00:02:41 +0100

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -21,9 +21,10 @@ export function log(message: string, source = "express") {
 export async function setupVite(app: Express, server: Server) {
   // ✅ moved dynamic import here
   const viteConfig =
-  process.env.NODE_ENV === "production"
-    ? {}
-    : ((await import("../vite.config.js")) as any).default;
+    process.env.NODE_ENV === "production"
+      ? {}
+      : ((await import("../vite.config.ts")) as any).default;
+
 
 
   const serverOptions = {
